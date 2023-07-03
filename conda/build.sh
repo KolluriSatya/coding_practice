096b7ef00b924eef34c0ca73565f5c24bf9bc3d9
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-03 12:52:11 +0200

    plasmidfinder

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,28 @@
-BINARIES="kma kma_index kma_shm kma_update"
-make CFLAGS="-w -O3 -I$PREFIX/include -L$PREFIX/lib"
+#!/bin/bash
 
+# Update default DB location
+PLASMIDFINDER_DB=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/database
+sed -i "s=BIOCONDA_SED_REPLACE=$PLASMIDFINDER_DB=" plasmidfinder.py
+
+# Copy plasmidfinder
 mkdir -p ${PREFIX}/bin
-cp $BINARIES $PREFIX/bin
-mkdir -p $PREFIX/doc/kma
-cp README.md $PREFIX/doc/kma/
+chmod +x plasmidfinder.py
+cp plasmidfinder.py ${PREFIX}/bin/plasmidfinder.py
+
+# copy script to download database
+chmod +x ${RECIPE_DIR}/download-db.sh
+cp ${RECIPE_DIR}/download-db.sh ${PREFIX}/bin/download-db.sh
+
+# Build database
+mkdir -p ${PLASMIDFINDER_DB}
+bash -x ${PREFIX}/bin/download-db.sh ${PLASMIDFINDER_DB}
+
+# set PLASMID_DB variable on env activation
+mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
+cat <<EOF >> ${PREFIX}/etc/conda/activate.d/plasmidfinder.sh
+export PLASMID_DB=${PLASMIDFINDER_DB}
+EOF
+
+cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/plasmidfinder.sh
+unset PLASMID_DB
+EOF
