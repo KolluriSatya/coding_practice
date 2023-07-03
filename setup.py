4c7af352bb3aa3abc1c22de88c74e6e3c26e81cc
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-03 09:12:54 +0200

    mintyper_release_3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Mintyper',
+      version='0.1.3',
+      description='Python Distribution Utilities',
+      author='satyakp',
+      author_email='gward@python.net',
+      url='https://www.github.com/MBHallgren/mintyper',
+      packages=find_packages(),
+      )
