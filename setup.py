04593fd955672ebbe141a0b0f42cd7be9e438c0e
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-05 10:49:42 +0200

    plasmidfinder2

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='plasmidfinder',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    data_files=[],
+    include_package_data=True,
+    url='https://https://github.com/genomicepidemiology/plasmidfinder',
+    license='',
+    install_requires=(),
+    author='Malte B. Hallgren',
+    scripts=['plasmidfinder.py'],
+    author_email='malhal@food.dtu.dk',
+    description='plasmidfinder - Plasmidfinder',
+)
