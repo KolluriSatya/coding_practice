b91dfaf07bb983c75b0f0f3ba7aa08dc5a89a3fc
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-19 12:56:19 +0200

    evergreen_9

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,8 @@
 
 mkdir -p ${PREFIX}/bin
 
-conda env create --file evergreen/scripts/environment.yml
+chmod +x parallel_snp_pipeline.py
+cp parallel_snp_pipeline.py  ${PREFIX}/bin/parallel_snp_pipeline.py
 
 # copy script to download database
 chmod +x ${RECIPE_DIR}/download-evergreen-db.sh
