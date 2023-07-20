46d3c8ec1ea2bc02deb4c62cf5097f96bfab15b6
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-20 17:17:48 +0200

    evergreen_30

---
+++ scripts/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='evergreen',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    data_files=[],
+    include_package_data=True,
+    url='https://https://github.com/genomicepidemiology/evergreen',
+    license='',
+    install_requires=(),
+    author='Malte B. Hallgren',
+    scripts=['scripts/parallel_snp_pipeline.py'],
+    author_email='malhal@food.dtu.dk',
+    description='parallel_snp_pipeline.py -h',
+)
