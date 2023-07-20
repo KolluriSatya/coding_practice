32e1e043dd203e8f9ade69e7f8a12701c20994b6
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-20 16:04:25 +0200

    evergreen_29

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-HR_DATABASE=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/database
-sed -i "s=BIOCONDA_SED_REPLACE=$HR_DATABASE=" parallel_snp_pipeline.py
-
 #copy evergreen
 mkdir -p ${PREFIX}/bin
 cd ..
@@ -13,17 +10,3 @@ cp parallel_snp_pipeline.py ${PREFIX}/bin/parallel_snp_pipeline.py
 # copy script to download database
 chmod +x ${RECIPE_DIR}/download-evergreen-db.sh
 cp ${RECIPE_DIR}/download-evergreen-db.sh ${PREFIX}/bin/download-evergreen-db.sh
-
-# Build database
-mkdir -p ${HR_DATABASE}
-bash -x ${PREFIX}/bin/download-db.sh ${HR_DATABASE}
-
-# set PLASMID_DB variable on env activation
-mkdir -p ${PREFIX}/etc/conda/activate.d ${PREFIX}/etc/conda/deactivate.d
-cat <<EOF >> ${PREFIX}/etc/conda/activate.d/parallel_snp_pipeline.sh
-export EVERGREEN_DB =${HR_DATABASE}
-EOF
-
-cat <<EOF >> ${PREFIX}/etc/conda/deactivate.d/parallel_snp_pipeline.sh
-unset EVERGREEN_DB
-EOF
