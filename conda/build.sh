55ba743717059f4663b29fc5dfcd94f01fa34d27
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-20 10:28:57 +0200

    evergreen_14

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ${PREFIX}/bin
 
-chmod +x  ./scripts/parallel_snp_pipeline.py
+chmod +x ./scripts/parallel_snp_pipeline.py
 cp ./scripts/parallel_snp_pipeline.py  ${PREFIX}/bin/parallel_snp_pipeline.py
 
 # copy script to download database
