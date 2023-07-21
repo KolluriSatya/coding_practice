d021686b116a49c826caa322008e898d97c8da4c
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-21 10:13:31 +0200

    evergreen_37

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
+if __name__ == '__main__':
 setup(
     name='evergreen',
     long_description=long_description,
