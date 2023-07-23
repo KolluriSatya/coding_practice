cd3ccd1d32382460198741dd92b1a28f6d3ba51f
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-23 12:23:58 +0200

    evergreen_43

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-
+BINARIES= "parallel_snp_pipeline"
+make CFLAGS="-w -03 -PREFIX/include -L$PREFIX/lib"
 mkdir -p ${PREFIX}/bin
-chmod +x *.py
-cp *.py ${PREFIX}/bin/
+cp $BINARIES ${PREFIX}/bin
+mkdir -p $PREFIX/doc/evergreen
 
