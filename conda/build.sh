812e3779292c3d14dcbe000bb8a08f5971d5d1c5
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-24 08:38:46 +0200

    evergreen

--- conda/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ${PREFIX}/bin
-chmod +x /home/satya/coding_practice/scripts/assimpler_pipeline.py
-
-cp /home/satya/coding_practice/scripts/assimpler_pipeline.py ${PREFIX}/bin/assimpler_pipeline.py
