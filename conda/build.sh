4de455ab232da881b4fac889f9f6c12f2ff5f32a
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-26 21:25:41 +0200

    flankophile

--- conda/build.sh
+++ conda/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+conda pack --prefix /opt/conda/conda-bld/flankophile_1690397023826/_build_env --output "$BUILD_DIR/flankophile.git"
 mkdir -p ${PREFIX}/bin
 cd /bin/abricate
 chmod 777 /bin//gff_to_gggenes.py
