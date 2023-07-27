7657f8ca1846eac87cfe3ad38b573841afa7ce84
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-27 08:09:36 +0200

    flankophile

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ${PREFIX}/bin
-cd /bin/abricate
-chmod 777 /bin//gff_to_gggenes.py
-sudo cp /bin//gff_to_gggenes.py  ${PREFIX}/bin/gff_to_gggenes.py
+chmod 777 gff_to_gggenes.py
+cp gff_to_gggenes.py  ${PREFIX}/bin/gff_to_gggenes.py
