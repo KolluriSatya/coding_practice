976d0b4ea90ffb2586fb62f2866b1dcf8ffe03d6
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-07-27 10:15:57 +0200

    evergreen

--- conda/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source ${PREFIX}/bin/activate
-mkdir -p ${PREFIX}/bin
-chmod +x parallel_snp_pipeline.py
-cp parallel_snp_pipeline.py ${PREFIX}/bin/parallel_snp_pipeline.py
