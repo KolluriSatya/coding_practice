4436e9d9b29777292deca0fbfaa56f4c6eb13480
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-08-15 10:54:28 +0200

    evergreen chk

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
-if __name__ == '__main__':
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
  setup(
     name='evergreen',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
     data_files=[],
     include_package_data=True,
     url='https:https://github.com/KolluriSatya/coding_practice/tree/evergreen',
     license='',
     install_requires=(),
     author='Malte B. Hallgren',
-    scripts=['scripts/parallel_snp_pipeline.py'],
-    description='evergreen' 'parallel_snp_pipeline',
+    scripts=['scripts/'],
+    description='evergreen',
 )
