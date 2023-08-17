11c2fee8c5857b8cab4a1b94c8759a46cc8231a2
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-08-17 12:15:47 +0200

    flankophile chk

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='flankophile',
+    packages=find_packages(),
+    data_files=[],
+    include_package_data=True,
+    url='https://https://github.com/genomicepidemiology/flankophile',
+    license='',
+    install_requires=(),
+    author='Malte B. Hallgren',
+    scripts=['bin/'],
+    author_email='malhal@food.dtu.dk',
+    description='flankophile', 'snakamake',
+)
