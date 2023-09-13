ac221ecba2c73851f897afb5e834bed5da5ce39e
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-09-13 09:57:59 +0200

    kma chk

--- conda/make_conda.py
+++ conda/make_conda.py
@@ -13,8 +13,8 @@ build['number'] = 0
 
 requirements = yaml.comments.CommentedMap()
 requirements['build'] = ['make', '{{ compiler(\'c\') }}']
-requirements['host'] = ['zlib', 'libzlib=1.2.13']
-requirements['run'] = ['zlib', 'libzlib=1.2.13']
+requirements['host'] = ['zlib', 'libzlib']
+requirements['run'] = ['zlib', 'libzlib']
 
 about = yaml.comments.CommentedMap()
 about['home'] = 'https://bitbucket.org/genomicepidemiology/kma'
