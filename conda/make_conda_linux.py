399ee4b3cd1e5bbfdc26bb453b7b73ea8adf5413
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-09-28 13:27:11 +0200

    flankophile test

--- conda/make_conda_linux.py
+++ conda/make_conda_linux.py
@@ -17,8 +17,8 @@ build['number'] = 1
 build['noarch'] = 'generic'
 
 requirements = yaml.comments.CommentedMap()
-requirements['host'] = ['python>=3.5', 'kma', 'wget', 'blast', 'biopython', 'cgecore']
-requirements['run'] = ['python>=3.5', 'kma', 'blast', 'biopython', 'cgecore']
+requirements['host'] = ['python>=3.5', 'kma', 'wget', 'biopython', 'blast', 'cgecore']
+requirements['run'] = ['python>=3.5', 'kma', 'biopython', 'blast', 'cgecore']
 
 about = yaml.comments.CommentedMap()
 about['home'] = 'https://bitbucket.org/genomicepidemiology/plasmidfinder'
