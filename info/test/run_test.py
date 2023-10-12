dcc575aa2e349289d6dbddb2b18f93c22d525aa4
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-10-12 10:39:27 +0200

    chk cgecore

---
+++ info/test/run_test.py
@@ -0,0 +1,24 @@
+print("import: 'cgecore'")
+import cgecore
+
+print("import: 'cgecore.blaster'")
+import cgecore.blaster
+
+print("import: 'cgecore.organisminfo'")
+import cgecore.organisminfo
+
+print("import: 'cgecore.alignment'")
+import cgecore.alignment
+
+print("import: 'cgecore.argumentparsing'")
+import cgecore.argumentparsing
+
+print("import: 'cgecore.cgefinder'")
+import cgecore.cgefinder
+
+print("import: 'cgecore.cmdline'")
+import cgecore.cmdline
+
+print("import: 'cgecore.utility'")
+import cgecore.utility
+
