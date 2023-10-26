a4d6aea5a7a66d12b1bfe8faca4bf5984ed78f0f
KolluriSatya <satyakprasannakolluri1995@gmail.com>
2023-10-26 13:07:05 +0200

    cgecore chk

--- conda/make_conda_linux.py
+++ conda/make_conda_linux.py
@@ -34,8 +34,7 @@ data['about'] = about
 data['extra'] = extra
 
 # Serialize the data to YAML and print it
-yaml_str = yaml.dump(data, Dumper=yaml.RoundTripDumper).replace("\"{{", "{{").replace("}}\"", "}}")
-yaml_str = yaml.dump(type="safe")
+yaml_str = yaml.dump(data, type=yaml.RoundTripsafe).replace("\"{{", "{{").replace("}}\"", "}}")
 print(yaml_str)
 
 with open('conda/meta.yaml', 'w') as f:
